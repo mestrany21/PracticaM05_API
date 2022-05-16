85dd136e963eb41e6f49eccf7c62233dfa24f324
mestrany21 <mestrany21@ilg.cat>
2022-05-16 17:07:12 +0200

    commit final

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get("/products", (req,res) => {
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/users', isAuthorized, (req,res) => {
+  res.json([{
+    id: 1,
+    name: 'Meritxell'
+  }])
+ })
+
+ app.get("/products", (req,res) => {
   const products = [
   {
     id: 1,
-    name: "hammer",
+    name: "poma",
   },
   {
     id: 2,
-    name: "screwdriver",
+    name: "pera",
   },
   ,
   {
     id: 3,
-    name: "wrench",
+    name: "platan",
   },
  ];
 
  res.json(products);
 });
-  
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+function isAuthorized(req,res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'contrasenya') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+}
